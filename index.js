db2b2cc794ab400737a6bca8471147f11b5805e7
ArturoVaccari <vaccarturo@gmail.com>
2019-04-19 10:20:20 +0200

    node process

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const http = require('http');
+const port =8082;
+
 
 const server = http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Nome Cognome<h1>');
-    res.end();
+    console.log(req.url)
+    if (req.url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>Home</h1>');
+        res.end();
+        } else if (req.url === '/api') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(JSON.stringify(
+            [
+                {
+                    "nome": "Mario",
+                    "cognome": "Rossi"
+                },
+                {
+                    "nome": "Ken",
+                    "cognome": "Norris"
+                },
+            ]            
+        ));
+        res.end();
+    }
+    else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<h1>${req.url}</h1>');
+        res.end();
+    }
+
 })
 
-server.listen(8081, '127.0.0.1');
+server.listen(port, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:8081/');
+console.log(`Server running at http://127.0.0.1:${port}`);
