c44d50bd80566cff809d6e36fd2bfcfefd405cae
ArturoVaccari <vaccarturo@gmail.com>
2019-05-07 11:50:50 +0200

    Ricerche per parametri

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const {settings} = require('./settings.js')
 const users = require('./routes/users')
 const personaggi = require('./routes/personaggi')
 let port = process.argv[2] || 8080
-app.use('/users', users)
-app.use('/personaggi', personaggi)
+
+const myLogger = (req, res, next) => {
+    console.log('LOGGED');
+    next();
+}
+
+app.use(myLogger);
+
+app.use('/v0.1/users', users)
+app.use('/v0.1/personaggi', personaggi)
 
 app.listen(port)
