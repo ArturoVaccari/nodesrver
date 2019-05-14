5fb5e4e6b542d4e8681df6c1851fc32fc6329e8c
ArturoVaccari <vaccarturo@gmail.com>
2019-05-14 09:49:25 +0200

    Cleaned project

--- index.js
+++ index.js
@@ -4,57 +4,7 @@ const users = require('./routes/users')
 const personaggi = require('./routes/personaggi')
 let port = process.argv[2] || 8080
 
-/*
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require("mongodb").ObjectId;
-const uri = "mongodb+srv://Vaccari:Vaccari@cluster0-7hppq.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
 
-client.connect(err => {
-    if(err) {
-        console.log('Error occured while connectrig to MongoDB Atls...\n', err);
-    }
-    console.log("connected")
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-
-codice ridondante
-  //read
-  client.db("PrimaProva").collection("Data").find().toArray(function(err, result){
-      if (err) throw err
-      console.log(result)
-  })
-  //insert
-  client.db("PrimaProva").collection("Data", function(err,collection){
-    const mario = { name: "Mario", lastname: "Rossi"};
-    collection.insertOne(mario);
-
-    client.db("PrimaProva").collection("Data").countDocuments(function(err, count){
-        if (err) throw err;
-        console.log("Total Rows: " + count);
-    });
-  });
-  //update
-  client.db("PrimaProva").collection("Data", function(err,collection){
-      const newOne = {name: "Pippo", lastname: "Stark", vivo: false};
-      collection.updateOne({_id:ObjectId("5cd54ae48630ed313874a65e")}, {$set: newOne},
-      function(err, result){
-          if(err) throw err;
-          console.log("Document Updated Successfully");
-      });
-  });
-  //delete
-  client.db("PrimaProva").collection("Data").remove({_id:ObjectId("5cd54ae48630ed313874a65e")}, {w:1}, function (err, result){
-    if (err) throw err;
-    console.log(`Document removed successfuly: ${result}`);
-  });
-  
-
-
-  client.close();
-});
-
- */
 
 app.use(express.urlencoded({extended: false}))
 
--- routes/users.js
+++ routes/users.js
@@ -4,57 +4,8 @@ const characters = require('../data/characters')
 // QUery
 // chiedi http://localhost:7070/personaggi?colore=giallo&sesso=m
 router.get('/', (req, res, next) => {
-    const query = req.query
-
-    let personaggi = characters.characters
-    if ('lastname' in query) {
-      personaggi = personaggi.filter((personaggio) => {
-        return personaggio.lastname === query.lastname
-      })
-    }
-    if ('firstname' in query) {
-      personaggi = personaggi.filter((personaggio) => {
-        return personaggio.firstname === query.firstname
-      })
-    }
-    res.send(personaggi)
-    // next();
-  }, (req, res, next) => {
-    console.log('FIRE 2')
-  }
-)
-
-// Params
-//http://localhost:7070/personaggi/9
-
-router.get('/:id', (req, res) => {
-  // convert string in numebr
-  const id = Number(req.params.id)
-  res.json(characters.characters.filter(item => item.id === id))
-})
-
-// Metodo POST
-router.post('/form', (req, res) => {
-  const body = req.body
-  const {
-    nome,
-    cognome,
-    occupazione,
-    tel
-  } = req.body
-
-  const status = {}
-  console.log(nome, cognome, occupazione, tel)
-  if (nome) {
-    status.code = 'ok'
-    status.message = `Benvenuto ${nome}`
-  } else {
-    status.code = 'error'
-    status.message = 'nome non valido'
-    status.campo = 'nome'
-  }
-  res.send(status)
-})
+    res.send("ciao utenti")
+  });
 
 
 module.exports = router
